eed9c95c5469012a4ba2c7f7ae834ee90bdf13a7
Olga <80950029+olgabelka81@users.noreply.github.com>
2023-12-12 18:03:16 +0300

    вносит финальные правки

--- components/data_cars_db.php
+++ components/data_cars_db.php
@@ -1,30 +1,26 @@
 <?php
-function returnValues($race_json_decode)
-{
-    return (array_values($race_json_decode));
+function get_decoding($json) {
+  $json_content = file_get_contents($json);
+  $json_decode = json_decode($json_content, true);
+  return ($json_decode);
+}
+
+$race_participants =  get_decoding('data_cars.json');
+$attempts = get_decoding('data_attempts.json');
+$max_count = 0;
+foreach ($attempts as $attempt) {
+  $sort_attempts[$attempt['id']]['id'] = $attempt['id'];
+  $sort_attempts[$attempt['id']]['results'][] = $attempt['result'];
+  if (count($sort_attempts[$attempt['id']]['results']) > $max_count) {
+    $max_count = count($sort_attempts[$attempt['id']]['results']); 
+   };
 };
 
-function get_race_participants() {
-    $cars_json_content = file_get_contents('data_cars.json');
-    $race_json_decode = json_decode($cars_json_content, true);
-    $race_participants = returnValues($race_json_decode);
-    return $race_participants;
+$dash = '<span style="font-weight: bold">-</span>';
+foreach ($race_participants as &$race_participant) {
+  $race_participant += $sort_attempts[$race_participant['id']];
+  if (count($race_participant['results']) < $max_count) {
+    array_push($race_participant['results'], $dash);
+  };
+  $race_participant['total'] = array_sum($race_participant['results']); 
 };
-
-$attempts_json_content = file_get_contents('data_attempts.json');
-$attempts = json_decode($attempts_json_content, true);
-
-$new_race_participants = [];
-$total = [];
-foreach (get_race_participants() as $new_race_participant):    
-    $results = [];
-    foreach ($attempts as $attempt):
-        if ($new_race_participant['id'] === $attempt['id']):
-            $results [] = $attempt['result'];  
-        endif;
-    endforeach; 
-    $new_race_participant['result'] = $results;
-    $new_race_participant['total'] = array_sum($results);
-    array_push($new_race_participants, $new_race_participant);
-    array_push($total, $new_race_participant['total']); 
-endforeach;
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require('components/db.php');
+require('components/data_cars_db.php');
 require('components/header.php');
 require('components/new_table_list.php');
 require('components/footer.php');
